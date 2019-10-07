8e6f27fe35626dec3212be0403e962290cd345cd
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-07 14:23:11 -0400

    initial commit

--- calculate.js
+++ calculate.js
@@ -1,6 +1,34 @@
-function calculate() {
-  
+function calculate(num,num1,sign) {
+    const realNum = Number(num);
+    const realNum1 = Number(num1);
+    if(sign === "+") {
+        return realNum + realNum1
+    } else if(sign === "-") {
+        return realNum - realNum1
+    } else if(sign === "x") {
+        return realNum * realNum1
+    } else if (sign === "/") {
+        return realNum / realNum1 
+    } else if(sign === "%") {
+        return realNum % realNum1
+    }else if(sign === "plus") {
+        return realNum + realNum1
+    } else if(sign === "minus") {
+        return realNum - realNum1
+    } else if( sign === "times") {
+        return realNum * realNum1
+    } else if(sign === "X") {
+        return realNum * realNum1
+    } else if(sign === "modulus") {
+        return realNum % realNum1
+    } else if(sign === "mod") {
+        return realNum % realNum1
+    } else (sign !== "+") {
+        return 'Sorry, thats not a mathematical operation!'
+    }
+    
+    
 }
-
+console.log(calculate("num", "num", "*")) 
 
 module.exports = calculate;
